69b54bf87e41ddd63b1c5d724e6105ae1915a281
unknown <natheerah24@gmail.com>
2022-09-28 10:38:52 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about pa-6">
+    <h1>About vuetify Todo</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat placeat, temporibus libero eligendi beatae sint ducimus aliquid omnis quidem doloremque hic suscipit nihil quos consequuntur autem blanditiis ut veritatis veniam?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat placeat, temporibus libero eligendi beatae sint ducimus aliquid omnis quidem doloremque hic suscipit nihil quos consequuntur autem blanditiis ut veritatis veniam?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat placeat, temporibus libero eligendi beatae sint ducimus aliquid omnis quidem doloremque hic suscipit nihil quos consequuntur autem blanditiis ut veritatis veniam?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat placeat, temporibus libero eligendi beatae sint ducimus aliquid omnis quidem doloremque hic suscipit nihil quos consequuntur autem blanditiis ut veritatis veniam?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat placeat, temporibus libero eligendi beatae sint ducimus aliquid omnis quidem doloremque hic suscipit nihil quos consequuntur autem blanditiis ut veritatis veniam?</p>
+  </div>
+</template>
